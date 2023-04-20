d602ce8351c8ba6485a0a4e658726039ad1e3a10
ruimin <65384356+ruimin72@users.noreply.github.com>
2023-04-20 15:52:55 +0800

    test logo change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import logo512 from './logo512.png';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src = {logo512} alt="logo" />
         <h1>Hello from V2</h1>
       </header>
     </div>
